68fabd493f85f1ecaa90659c602da314182eac8b
Shleif119 <107221242+Shleif119@users.noreply.github.com>
2024-08-06 23:59:25 +0300

    Гпт текст с заданием передается рандомним файлом для каждого пользователя

--- gpt4free.php
+++ gpt4free.php
@@ -2,29 +2,31 @@
 
 $condition = "Відповідь давай тільки українською мовою. Є завдання написати програму з такою умовою:";
 
-$code_program = "Ось код програми, вірно написана програма ? відповідає вона завданню ? ВАЖЛИВО!!! Не пиши код вірної програми";
+$code_program = "Ось код програми, вірно написана програма ? відповідає вона завданню ? ВАЖЛИВО!!! Не пиши код вірної програми.не використовуй кітайську мову у відповідях";
 
 $userCode = $_POST["user_code"];
 
 // Объединяем строки
 $combinedText = $condition . "\n" . $progaText . "\n" . $userCode . "\n" . $code_program;
 
-// Записываем в текстовый файл
-$inputFile = 'input.txt';
-file_put_contents($inputFile, $combinedText);
 
-// Запуск Python-скрипта
+// Имя временного файла
+$inputFileName = "input_gpt" . $userLogin . ".txt";
+
+// Убедитесь, что текст закодирован в UTF-8
+$encodedText = mb_convert_encoding($combinedText, 'UTF-8', 'auto');
+
+// Запись данных в файл
+file_put_contents($inputFileName, $encodedText);
+
+// Имя Python-скрипта
 $pythonScript = 'gpt4free.py';
-$output_gpt = shell_exec("python $pythonScript 2>&1");
 
-// // Запись результата выполнения Python-скрипта в файл
-// $outputFile = 'output.txt';
-// file_put_contents($outputFile, $output_gpt);
+// Запуск Python-скрипта и передача данных через файл
+$output_gpt = shell_exec("python $pythonScript < $inputFileName");
 
-// // Вывод результата
-// echo "Текст успешно записан в файл $inputFile<br>";
-// echo "Результат выполнения Python-скрипта записан в файл $outputFile<br>";
+
 
 echo $output_gpt;
-
+unlink("input_gpt{$userLogin}.txt");
 ?>
--- gpt4free.py
+++ gpt4free.py
@@ -4,14 +4,16 @@ from g4f.client import Client
 import sys
 import io
 
-# Встановлення потрібного кодування
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 # Установка политики цикла событий
 asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
 
-# Чтение содержимого файла
-with open('input.txt', 'r', encoding='utf-8') as file:
-    file_content = file.read()
+# Убедитесь, что стандартный ввод и вывод работают с UTF-8
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+
+# Чтение данных из стандартного ввода
+file_content = sys.stdin.read()
 
 # Создание клиента и отправка запроса
 client = Client()
@@ -22,8 +24,6 @@ response = client.chat.completions.create(
 
 # Получение содержимого ответа
 response_content = str(response.choices[0].message.content)  # Преобразование в строку
-
-
 
 
 # Печать для проверки (опционально)
